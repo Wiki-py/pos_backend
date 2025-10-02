ca4ffe0835d15857e2f659a6e2faa2d64f147de0
wiki-py <muhumuzawycliffcc@gmail.com>
2025-10-02 14:24:12 +0300

    updated user.urls.py

--- bms_backend/apps/users/urls.py
+++ bms_backend/apps/users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+    path('users/profile/', UserViewSet.as_view({'get': 'profile'}), name='user-profile')
+
     
 ]
